72cf249b52d0ff39630e254c185d623bed08c52c
amir <amirsharapov.personal@gmail.com>
2024-03-02 01:48:03 -0600

    Updated constants

--- app/src/main/java/com/example/androidclient/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.androidclient;
-
-public class Constants {
-    public static String TAG = "androidclient";
-    public static String NOTIFICATION_CHANNEL_ID = "androidclient";
-}
---
+++ app/src/main/java/com/example/androidclient/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.androidclient
+
+object Constants {
+    var TAG = "androidclient"
+    var NOTIFICATION_CHANNEL_ID = "androidclient"
+}
